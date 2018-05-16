95c42126e14946038b014d9a67dcc074991b1335
sunshun <ssnoodles0226@gmail.com>
2018-05-16 09:16:21 +0800

    edit osc service

--- src/main/java/cc.ssnoodles.sync/Content.java
+++ src/main/java/cc.ssnoodles.sync/Content.java
@@ -12,18 +12,68 @@ import lombok.Setter;
 @Setter
 @Getter
 public class Content {
-    private String draft;
+    /**
+     * 文章ID 1511470
+     */
+    private Long id;
+    /**
+     * 是否是草案 有时会是数字
+     */
+    private String draft = "false";
+    /**
+     * 用户编码
+     */
     private String user_code;
-    private String catalog;
+    /**
+     * 目录 分类 JAVA 3711389 SCALA 5694873
+     */
+    private int catalog = 3711389;
+    /**
+     * 标题
+     */
     private String title;
-    private String content_type;
+    /**
+     * 3 markdown 4 富文本
+     */
+    private int content_type = 3;
+    /**
+     * 标签 逗号分隔
+     */
     private String tags;
-    private String classification;
-    private String type;
-    private String privacy;
-    private String deny_comment;
-    private String as_top;
-    private String isRecommend;
+    /**
+     * 系统分类 428609 编程语言
+     */
+    private String classification = "428609";
+    /**
+     * 未知
+     */
+    private int type = 1;
+    /**
+     * 是否对所有人可见 0
+     */
+    private int privacy = 0;
+    /**
+     * 是否允许评论 0
+     */
+    private int deny_comment = 0;
+    /**
+     * 是否置顶 0
+     */
+    private int as_top = 0;
+    /**
+     * on
+     */
+    private String isRecommend = "on";
+    /**
+     * 摘要
+     */
+    private String abstracts;
+    /**
+     * 内容
+     */
     private String content;
-    private String temp;
+    /**
+     * 时间戳
+     */
+    private long temp = System.currentTimeMillis();
 }
--- src/main/java/cc.ssnoodles.sync/OscBlogService.java
+++ src/main/java/cc.ssnoodles.sync/OscBlogService.java
@@ -4,6 +4,8 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.http.Header;
 import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpStatus;
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 import cn.hutool.setting.dialect.Props;
@@ -20,24 +22,29 @@ import java.util.List;
 public class OscBlogService {
     private static final Props PROPS = new Props("app.properties");
     private static final Log LOG = LogFactory.get();
-    private static final String OSCID = "oscid";
+    private static final String LOGIN_URL = "https://www.oschina.net/action/user/hash_login?from=";
+    private static final String SAVE_URL = "https://my.oschina.net/action/blog/save";
+    private static final String EDIT_URL = "https://my.oschina.net/action/blog/edit";
     /**
      * 登陆
      */
     public String login() {
         String email = PROPS.getStr("osc.email");
         String pwd = PROPS.getStr("osc.pwd");
-        String loginUrl = PROPS.getStr("osc.loginUrl");
 
         HashMap<String, Object> paramMap = new HashMap<>(2);
         paramMap.put("email", email);
         paramMap.put("pwd", pwd);
-        List<HttpCookie> httpCookies = HttpRequest.post(loginUrl)
+        HttpResponse response = HttpRequest.post(LOGIN_URL)
                 .form(paramMap)
-                .execute().getCookie();
-        if (CollUtil.isNotEmpty(httpCookies)) {
-            return httpCookies.get(0).toString();
+                .execute();
+        List<HttpCookie> httpCookies = response.getCookie();
+        if (HttpStatus.HTTP_OK == response.getStatus() && CollUtil.isNotEmpty(httpCookies)){
+            String cookie = httpCookies.get(0).toString();
+            LOG.debug("login success, cookie : {}", cookie);
+            return cookie;
         }
+        LOG.error("login failed ! this email : {}", email);
         return null;
     }
 
@@ -47,14 +54,23 @@ public class OscBlogService {
      * @param content
      * @return
      */
-    public String saveBlog(String cookie, Content content) {
-        String saveBlogUrl = PROPS.getStr("osc.saveBlogUrl");
-
-        String body = HttpRequest.post(saveBlogUrl)
+    public int saveBlog(String cookie, Content content) {
+        return HttpRequest.post(SAVE_URL)
                 .header(Header.COOKIE, cookie)
                 .form(BeanUtil.beanToMap(content))
-                .execute().body();
+                .execute().getStatus();
+    }
 
-        return null;
+    /**
+     * 修改博客
+     * @param cookie
+     * @param content
+     * @return
+     */
+    public int editBlog(String cookie, Content content) {
+        return HttpRequest.post(EDIT_URL)
+                .header(Header.COOKIE, cookie)
+                .form(BeanUtil.beanToMap(content))
+                .execute().getStatus();
     }
 }
